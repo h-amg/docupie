71f587bcc29724cd8b1dea40c25c063cb92f0540
Husam Gibreel <husamgibreel@gmail.com>
2025-01-20 15:01:23 +0400

    - Update: package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docupie",
-    version="0.1.0",
+    version="0.1.1",
     description="An advanced document processing tool that leverages AI to extract structured data from PDFs",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
